c44582a130f674b36c24acfe495cb8a9b8d1be70
anbarasanv <anbarasan.mailme@gmail.com>
2024-04-06 21:23:26 +0530

    added fonts and homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: ["./*.hbs", "./**/*.hbs"],
   theme: {
     extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
   },
   plugins: [require("daisyui")],
 };
